0e7cfbf1b4e4da8897b27eb5ebcaf18a06bd24c6
josnavch <josnavch@gmail.com>
2021-03-04 04:57:10 +0000

    Finish Exercise Traffic light JN

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	let [luzRed, setLuzRed] = useState(true);
+	let [luzYellow, setLuzYellow] = useState(false);
+	let [luzGreen, setLuzGreen] = useState(false);
+	const [lightOnRed, setlightRed] = useState("none");
+	const [lightOnYellow, setlightYellow] = useState("none");
+	const [lightOnGreen, setlightGreen] = useState("none");
+
+	function cambioRed() {
+		if (luzRed == false) {
+			setLuzRed(true);
+			setLuzYellow(false);
+			setLuzGreen(false);
+		} else {
+			setLuzRed(false);
+		}
+	}
+	function cambioYellow() {
+		if (luzYellow == false) {
+			setLuzYellow(true);
+			setLuzRed(false);
+			setLuzGreen(false);
+		} else {
+			setLuzYellow(false);
+		}
+	}
+	function cambioGreen() {
+		if (luzGreen == false) {
+			setLuzGreen(true);
+			setLuzRed(false);
+			setLuzYellow(false);
+		} else {
+			setLuzGreen(false);
+		}
+	}
+	useEffect(() => {
+		setlightRed(luzRed ? "selected" : "");
+		//	console.log(lightOnRed, luzRed, luzYellow, luzGreen);
+	}, [luzRed]);
+	useEffect(() => {
+		setlightYellow(luzYellow ? "selected" : "");
+		//	console.log(lightOnYellow, luzRed, luzYellow, luzGreen);
+	}, [luzYellow]);
+	useEffect(() => {
+		setlightGreen(luzGreen ? "selected" : "");
+		//	console.log(lightOnGreen, luzRed, luzYellow, luzGreen);
+	}, [luzGreen]);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div id="trafficTop"> </div>
+			<div className="container">
+				<div>
+					<button
+						className={"button red " + lightOnRed}
+						onClick={cambioRed}></button>
+				</div>
+				<div>
+					<button
+						className={"button yellow " + lightOnYellow}
+						onClick={cambioYellow}></button>
+				</div>
+				<div>
+					<button
+						className={"button green " + lightOnGreen}
+						onClick={cambioGreen}></button>
+				</div>
+			</div>
 		</div>
 	);
 }
